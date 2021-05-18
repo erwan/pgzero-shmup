34a182728b9eb4a548b8d26d86e6152fbd2878ce
Erwan Loisant <erwan@loisant.com>
2021-05-18 21:19:43 +0200

    wip

--- actors.py
+++ actors.py
@@ -1,12 +1,52 @@
-from pgzero.actor import Actor
+import pygame
+from pygame.locals import *
 from pygame.math import Vector2
+from pygame.sprite import Sprite
+
+WIDTH = 512
+HEIGHT = 256
+
+speed = 1
 
 
-class Ship(Actor):
-    def __init__(self, **kwargs):
-        super(Ship, self).__init__('ship/blue-01.png', **kwargs)
+class Bullet(Sprite):
+    def __init__(self, pos):
+        Sprite.__init__(self)
+        self.image = pygame.image.load('images/projectile.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = pos
+
+    def update(self):
+        self.rect.move_ip(4, 0)
+
+
+class Ship(Sprite):
+    def __init__(self, pos):
+        Sprite.__init__(self)
+        self.image = pygame.image.load('images/ship/blue-01.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = pos
+
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -4)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 4)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-4, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(4, 0)
+
+        # Keep player on the screen
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= HEIGHT:
+            self.rect.bottom = HEIGHT
 
     def fire(self):
-        bullet = Actor('projectile', pos=self.pos)
-        bullet.exact_pos = bullet.start_pos = Vector2(self.pos)
+        bullet = Bullet(pos=self.rect.center)
         return bullet
--- intro.py
+++ intro.py
@@ -1,53 +1,75 @@
 import pygame
+from pygame.locals import *
 from actors import Ship
 
 WIDTH = 512
 HEIGHT = 256
 
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
+
+running = True
+
 # Game state
-ship = Ship(pos=(50, 50))
+ship = Ship((50, 50))
+all_sprites.add(ship)
 bullets = []
 
 # Parameters
 speed = 4
 bullet_speed = 8
 
-music.play('war')
+# music.play('war')
 
 
-def update():
-    if keyboard.left:
-        ship.x -= speed
-    if keyboard.right:
-        ship.x += speed
-    if keyboard.up:
-        ship.y -= speed
-    if keyboard.down:
-        ship.y += speed
-    for b in bullets:
-        b.x += bullet_speed
-        if (b.x > WIDTH):
-            bullets.remove(b)
-
-
-def on_key_down(key):
-    if key == keys.SPACE:
-        sounds.shot03.play()
-        bullets.append(ship.fire())
-    if key == keys.ESCAPE:
-        exit()
+def input():
+    pressed_keys = pygame.key.get_pressed()
+    ship.update(pressed_keys)
+    if pressed_keys[K_SPACE]:
+        bullet = ship.fire()
+        bullets.append(bullet)
+        all_sprites.add(bullet)
 
 
 def draw():
-    #screen.surface = pygame.display.set_mode((WIDTH, HEIGHT), pygame.FULLSCREEN)
-    screen.clear()
-    screen.blit('bg/fantasy-512-x-256_006', (0, 0))
-    screen.blit('bg/fantasy-512-x-256_005', (0, 0))
-    screen.blit('bg/fantasy-512-x-256_004', (0, 0))
-    screen.blit('bg/fantasy-512-x-256_003', (0, 0))
-    screen.blit('bg/fantasy-512-x-256_002', (0, 0))
-    screen.blit('bg/fantasy-512-x-256_001', (0, 0))
-    screen.blit('bg/fantasy-512-x-256_000', (0, 0))
-    ship.draw()
+    bg = pygame.image.load('images/bg/fantasy-512-x-256_006.png')
+    screen.blit(bg, (0, 0))
+
+    all_sprites.draw(screen)
+    pygame.display.flip()
+
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Tick at 60 fps
+    clock.tick(60)
+
     for bullet in bullets:
-        bullet.draw()
+        bullet.update()
+
+    input()
+    draw()
+
+
+pygame.quit()
+
+
+# def update():
+#     for b in bullets:
+#         b.x += bullet_speed
+#         if (b.x > WIDTH):
+#             bullets.remove(b)
+
+
+# Def on_key_down(key):
+#     if key == keys.SPACE:
+#         sounds.shot03.play()
+#         bullets.append(ship.fire())
+#     if key == keys.ESCAPE:
+#         exit()
